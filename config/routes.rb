9676a8254a88662f49f15bc43759d23b383740d6
andela-oogunwa <fadekemi.ogunwa@andela.co>
2015-03-28 21:30:21 +0100

    added login image

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'login', to: 'home#login'
 
   root "home#index"
 
