dd6081c4cc1cfad3f0ac1f00cb12bd4ee59a6c4f
Adefila Sunday <sunday.adefila@andela.co>
2015-03-28 19:36:56 +0100

    added helper methods for authenticating signed in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+  # def sign_in(user)
+  #   cookies.permanent[:remember_token] = user.remember_token
+  #   self.current_user = user
+  # end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def authenticate_user
+    unless signed_in?
+      redirect_to root_path, notice: "Please sign in."
+    end
+  end
 end
