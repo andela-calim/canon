513fe6ecc0562b1a2e1984fb41c5264dff879809
Bello Babajide <bello.babajide@andela.co>
2015-03-29 20:46:47 +0100

    style changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329152326) do
+ActiveRecord::Schema.define(version: 20150329151012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,10 @@ ActiveRecord::Schema.define(version: 20150329152326) do
   create_table "questions", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "popularity", default: 0
   end
-
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
